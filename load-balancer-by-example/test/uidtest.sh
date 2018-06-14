2f0ae348fdbf04dabb9fcef35db198d5e46f74ce
William Yao <wmyao@emotibot.com.cn>
2018-06-14 23:28:18 +0800

    golang server and build script

--- load-balancer-by-example/test/uidtest.sh
+++ load-balancer-by-example/test/uidtest.sh
@@ -4,4 +4,4 @@ echo userId=$1
 
 curl  -X GET localhost:10080/index.html?userId=$1
 
-curl  -v -d "userId=$1" -X POST localhost:10080/index.html
+curl  -d "userId=$1" -X POST localhost:10080/index.html
